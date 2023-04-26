c88633161fde4a9141b62f6705c84cc27ec783e6
leandro.i.farias <leandro.inacio.farias@gmail.com>
2023-04-26 20:14:51 -0300

    Scenarios Login success and error

--- login_test.js
+++ login_test.js
@@ -1,5 +1,33 @@
 Feature('login');
 
-Scenario('test something',  ({ I }) => {
-    I.wait(5);
+Scenario('Login with success',  ({ I }) => {
+
+    // fill email field for 'accessibility id'
+    I.fillField('~email','teste@teste.com')
+
+    // fill email field
+    I.fillField('~senha','123456')
+
+    // tap on enter
+    I.tap('~entrar')
+
+    // check
+    I.waitForElement('~codigo', 5)
+    I.seeElement('~codigo')
+});
+
+Scenario('Login with error',  ({ I }) => {
+
+    // fill email field for 'accessibility id'
+    I.fillField('~email','teste@teste.com')
+
+    // fill email field
+    I.fillField('~senha','123')
+
+    // tap on enter
+    I.tap('~entrar')
+
+    // check
+    I.waitForElement('~lognFail', 5)
+    I.seeElement('~lognFail')
 });
